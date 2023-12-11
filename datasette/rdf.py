1740457a50345008bda2267f6923be03ae18da71
David Jones <davidthomjones@gmail.com>
2023-12-11 13:07:48 +1000

    misc update

--- datasette/rdf.py
+++ datasette/rdf.py
@@ -17,15 +17,155 @@
 Experiment in using rdflib to parse the v9 Oz curriculum files
 """
 
-from rdflib import Graph
+from pprint import pprint 
+
+from rdflib import Graph, URIRef, Literal, Namespace
 from rdflib.namespace import RDF
+from rdflib.plugin import register, Serializer, Parser
+
+def dumpGraph():
+    """
+    Simple text dump of graph
+    """
+
+    print(g.serialize( format="json-ld", indent=4))
+
+def printMyNameSpace(g):
+    """
+    Display the namespaces used in the graph
+    """
+    for ns_prefix, namespace in g.namespaces():
+        print(f"ns_prefix {ns_prefix} namespace {namespace}")
+
+def dumpContentDescriptions(g):
+    """
+    Given a graph for a Oz Curriculum RDF file extract out all the content descriptions and display their code and title
+
+    Content descriptions are identified by the 
+    - predicate http://purl.org/ASN/schema/core/statementLabel
+    - object 'Content Description'
+    """
+
+    cdGraph = extractContentDescriptions(g)
+    dumpGraph(cdGraph)
+
+def extractContentDescriptions(g):
+    """
+    Return a graph of all the content descriptions in the given graph
+    """
+    cdGraph = Graph()
+
+    #-- form the literal search
+    # - the language is important
+    cdLiteral = Literal("Content Description", lang="en-au")
+    # - namespace
+    asnNameSpace = Namespace("http://purl.org/ASN/schema/core/")
+    cdPredicate = asnNameSpace.statementLabel
+
+    cdGraph += g.triples((None, cdPredicate, cdLiteral))
+
+    return cdGraph
+
+def extractYearLevel(graph, yearLevel):
+    """
+    Return a graph of all statements associated with a given year level
+    - predicate URIRef('https://www.esa.edu.au/nominalYearLevel')
+    - object Literal(f"Year {yearLevel}", lang='en-au')
+    """
+
+    ylGraph = Graph()
+    
+    ylLiteral = Literal(f"Year {yearLevel}", lang='en-au')
+    ylPredicate = URIRef('https://www.esa.edu.au/nominalYearLevel')
+    
+    ylGraph += graph.triples((None, ylPredicate, ylLiteral))
+    
+    return ylGraph
+
+def getYearLevelContentDescriptionIds(graph, yearLevel):
+    """
+    Return a list of @ids into the graph for all the content descriptions that match the given yearLevel
+    """
+
+    #-- generate a graph for a given year level
+    yearLevelGraph = extractYearLevel(graph, yearLevel)
+
+    #-- get a list of subjects in that graph
+    subjects = [str(s) for s in yearLevelGraph.subjects()]
+    pprint(subjects)
+
+#    dumpGraph(yearLevelGraph)
+
+    #-- generate a graph of the content descriptions from that year level graph
+    cdGraph = extractContentDescriptions(yearLevelGraph)
+
+#    ylCdGraph = yearLevelGraph + cdGraph
+
+    dumpGraph(cdGraph)
+#    dumpGraphTriples(cdGraph)
+
+    #-- return a list of the @ids of the content descriptions 
+    #return [str(cd) for cd in cdGraph.subjects()]
+
+def dumpGraph(g):
+    """
+    Simple text dump of graph
+    """
+
+    print(g.serialize( format="json-ld", indent=4))
+
+def dumpGraphTriples(g):
+    """
+    Simple text dump of graph
+    """
+
+    for s, p, o in g:
+        print("----- subject")
+        pprint(s)
+        print("----- predicate")
+        pprint(p)
+        print("----- object")
+        pprint(o)
+
+def sparqlDump(g):
+    """
+    Experiment with using SPARQL to query the graph
+    Just do a simple dump
+    """
+
+    query = """
+    SELECT ?s ?p ?o
+    WHERE {
+        ?s ?p ?o .
+    }
+    LIMIT 3 
+    """
+
+    results = g.query(query)
+    for row in results:
+        pprint(row)
+    
+
 
 g = Graph()
+#g.parse("../data/v9/MAT.rdf", format="xml", bind_namespaces="rdflib")
 g.parse("../data/v9/MAT.rdf", format="xml")
+
+#dumpGraphTriples(g)
+#dumpContentDescriptions(g)
+
+#getYearLevelContentDescriptionIds(g, 10)
+
+sparqlDump(g)
+
+#dumpGraphTriples(g)
+quit(0)
+
+
+
 
 #print(len(g))
 
-import pprint 
 #pprint.pprint(g.subjects)
 
 subjects = {}
@@ -33,6 +173,13 @@ subjects = {}
 for s, p, o in g:
     if (s, p, o) not in g:
         raise Exception("It better be!")
+
+    print("----- subject")
+    pprint(s)
+    print("----- predicate")
+    pprint(p)
+    print("----- object")
+    pprint(o)
 
     #-- if the subject is not in the dictionary, add it
     if s not in subjects:
